6b4de9633550964fdb311a5d4ea737bcb0970021
Max Tao <maxtao989@gmail.com>
2020-10-18 03:18:09 -0700

    please work on correctly inputting user messages after first bot message

--- src/main/java/events/HelloEvent.java
+++ src/main/java/events/HelloEvent.java
@@ -5,12 +5,33 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Arrays;
+
 public class HelloEvent extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        if (event.getAuthor().isBot()){
+            return;
+        }
         String messageSent = event.getMessage().getContentRaw();
         if (messageSent.equalsIgnoreCase("-hello")){
-            event.getChannel().sendMessage("Hey there! What can I get started for you?").queue();
+            EmbedBuilder menu = new EmbedBuilder();
+            event.getChannel().sendMessage("Hey there! What can I get started for you?\n").queue();
+            menu.setTitle("MENU: \n");
+            menu.addField("Currently we have:", "Coffee\n Tea \n Bagel \n Chocolate", false);
+            event.getChannel().sendMessage(menu.build()).queue();
+
+
+            //figure out how to input messages from user after bot sends message (practically allow a short or full conversation between bot and user)
+            String message = event.getMessage().getContentRaw();
+
+
+            if (message.length() > 1){
+                event.getChannel().sendMessage("I'm sorry, it seems like what you ordered is not on our menu :frowning:").queue();
+            } else if (message.length() == 1 && message.equalsIgnoreCase("Coffee")){
+                event.getChannel().sendMessage(":coffee:").queue();
+            }
+
         }
 
 
