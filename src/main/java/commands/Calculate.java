38a664997532b8b826cba59fb84c9a416049a562
Max Tao <maxtao989@gmail.com>
2020-10-29 12:36:32 -0700

    allow more than 2 inputs for calculation

--- src/main/java/commands/Calculate.java
+++ src/main/java/commands/Calculate.java
@@ -3,9 +3,11 @@ package commands;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 
 public class Calculate extends ListenerAdapter {
+
 
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
@@ -17,40 +19,88 @@ public class Calculate extends ListenerAdapter {
             event.getChannel().sendMessage("To use this command, type(without brackets): !calculate " +
                     "[add/sub/mult/divide] [firstnum] [secondnum]").queue();
         } else if (message[0].equalsIgnoreCase("-calculate") && message[1].equalsIgnoreCase("add")){
-            double numberOne = 0;
-            double numberTwo = 0;
+
+            if (message.length == 3){
+                event.getChannel().sendMessage("You need to enter at least two numbers").queue();
+                return;
+            }
+            
             try {
-                numberOne = Double.parseDouble(message[2]);
-                numberTwo = Double.parseDouble(message[3]);
-                event.getChannel().sendMessage("The answer is " + (numberOne + numberTwo)).queue();
+                int a = 2;
+                double[] numbers = new double[message.length - 2];
+                while (a < message.length){
+                    numbers[a - 2] = Double.parseDouble(message[a]);
+                    a++;
+                }
+
+                double sum = 0;
+                for (int i = 0; i < numbers.length; i++){
+                    sum += numbers[i];
+                }
+                event.getChannel().sendMessage("The answer is " + sum).queue();
             } catch (ArrayIndexOutOfBoundsException e){
-                event.getChannel().sendMessage("Can you tell me the two numbers you want me to use?").queue();
+                event.getChannel().sendMessage("Can you tell me the numbers you want me to use?").queue();
+                return;
             } catch (NumberFormatException e){
                 event.getChannel().sendMessage("Just stop").queue();
+                return;
             }
+
+
+
         } else if (message[0].equalsIgnoreCase("-calculate") && message[1].equalsIgnoreCase("sub")){
-            double numberOne = 0;
-            double numberTwo = 0;
-            try {
-                numberOne = Double.parseDouble(message[2]);
-                numberTwo = Double.parseDouble(message[3]);
-                event.getChannel().sendMessage("The answer is " + (numberOne - numberTwo)).queue();
-            } catch (ArrayIndexOutOfBoundsException e){
-                event.getChannel().sendMessage("Can you tell me the two numbers you want me to use?").queue();
-            } catch (NumberFormatException e){
-                event.getChannel().sendMessage("Just stop").queue();
+            if (message.length == 3){
+                event.getChannel().sendMessage("You need to enter at least two numbers").queue();
+                return;
             }
-        } else if (message[0].equalsIgnoreCase("-calculate") && message[1].equalsIgnoreCase("mult")){
-            double numberOne = 0;
-            double numberTwo = 0;
+
             try {
-                numberOne = Double.parseDouble(message[2]);
-                numberTwo = Double.parseDouble(message[3]);
-                event.getChannel().sendMessage("The answer is " + (numberOne * numberTwo)).queue();
+                int a = 2;
+                double[] numbers = new double[message.length - 2];
+                while (a < message.length){
+                    numbers[a - 2] = Double.parseDouble(message[a]);
+                    a++;
+                }
+
+                double answer = numbers[0];
+                for (int i = 1; i < numbers.length; i++){
+                    answer -= numbers[i];
+                }
+                event.getChannel().sendMessage("The answer is " + answer).queue();
             } catch (ArrayIndexOutOfBoundsException e){
-                event.getChannel().sendMessage("Can you tell me the two numbers you want me to use?").queue();
+                event.getChannel().sendMessage("Can you tell me the numbers you want me to use?").queue();
+                return;
             } catch (NumberFormatException e){
                 event.getChannel().sendMessage("Just stop").queue();
+                return;
+            }
+
+
+        } else if (message[0].equalsIgnoreCase("-calculate") && message[1].equalsIgnoreCase("mult")){
+            if (message.length == 3){
+                event.getChannel().sendMessage("You need to enter at least two numbers").queue();
+                return;
+            }
+
+            try {
+                int a = 2;
+                double[] numbers = new double[message.length - 2];
+                while (a < message.length){
+                    numbers[a - 2] = Double.parseDouble(message[a]);
+                    a++;
+                }
+
+                double answer = 1;
+                for (int i = 0; i < numbers.length; i++){
+                    answer *= numbers[i];
+                }
+                event.getChannel().sendMessage("The answer is " + answer).queue();
+            } catch (ArrayIndexOutOfBoundsException e){
+                event.getChannel().sendMessage("Can you tell me the numbers you want me to use?").queue();
+                return;
+            } catch (NumberFormatException e){
+                event.getChannel().sendMessage("Just stop").queue();
+                return;
             }
         } else if (message[0].equalsIgnoreCase("-calculate") && message[1].equalsIgnoreCase("divide")){
             double numberOne = 0;
