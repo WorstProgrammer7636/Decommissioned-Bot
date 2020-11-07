9e24ef38bbc4bf9e14a21216a1548347c260a2a2
Max Tao <maxtao989@gmail.com>
2020-11-06 21:48:08 -0800

    separate shop from main currency

--- src/main/java/Currency/CurrencySystem.java
+++ src/main/java/Currency/CurrencySystem.java
@@ -3,7 +3,6 @@ package Currency;
 import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,8 +13,6 @@ import java.util.Random;
 import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
-import java.lang.NullPointerException;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 
@@ -25,7 +22,6 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.sharding.ShardManager;
 
 public class CurrencySystem extends ListenerAdapter {
     EventWaiter waiter = new EventWaiter();
@@ -79,31 +75,45 @@ public class CurrencySystem extends ListenerAdapter {
             }
         }
         // SEPERATE COMMANDS
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("-bal")) {
-            try {
-                long member = event.getMember().getIdLong();
+        String[] split = event.getMessage().getContentRaw().split("\\s+");
+        if (split[0].equalsIgnoreCase("-bal")) {
+            int length = split.length;
+            if (length >= 2) {
+                boolean found = false;
+                String name = "";
+                for (int j = 1; j < length; j++) {
+                    name += split[j];
+                    if (j < length - 1) {
+                        name += " ";
+                    }
+                }
+                int size = event.getGuild().getMembersByEffectiveName(name, true).size();
+                found = false;
+                for (int i = 0; i < size; i++) {
 
-                getMoney(guild, member, event.getChannel(), event.getMember());
-            } catch (IOException e) {
-                e.printStackTrace();
+                    found = true;
+                    try {
+                        getMoney(guild, event.getGuild().getMembersByEffectiveName(name, true).get(0).getIdLong(),
+                                event.getChannel(), event.getGuild().getMembersByEffectiveName(name, true).get(0));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (found == false) {
+                    event.getChannel()
+                            .sendMessage("Please type in the nickname of someone in the server! (Don't ping!)").queue();
+                }
+
+            } else {
+                try {
+                    long member = event.getMember().getIdLong();
+
+                    getMoney(guild, member, event.getChannel(), event.getMember());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } else if (event.getMessage().getContentRaw().startsWith("-shop")) {
-            eb.setTitle("Shop");
-            int R = (int) (Math.random() * 256);
-            int G = (int) (Math.random() * 256);
-            int B = (int) (Math.random() * 256);
-            Color color = new Color(R, G, B);
-            Random random = new Random();
-            final float hue = random.nextFloat();
-            final float saturation = 0.9f;
-            final float luminance = 1.0f;
-            color = Color.getHSBColor(hue, saturation, luminance);
-            eb.addField("Consumables:", "Coffee (500 coins) :coffee: \n *More Items Coming Soon*", false);
-            eb.setFooter("You have ten seconds to pick an item! Simply type in an object's name to get it!");
-            eb.setColor(color);
-            event.getChannel().sendMessage(eb.build()).queue();
-            Shop(event.getChannel().getIdLong(), event.getMember().getIdLong(), event.getJDA().getShardManager(),
-                    event.getMember(), event.getChannel());
         }
     }
 
@@ -201,7 +211,7 @@ public class CurrencySystem extends ListenerAdapter {
                 final float saturation = 0.9f;// 1.0 for brilliant, 0.0 for dull
                 final float luminance = 1.0f; // 1.0 for brighter, 0.0 for black
                 color = Color.getHSBColor(hue, saturation, luminance);
-                XP.addField("Coin Information", "You have **" + coins + "** coins.", false);
+                XP.addField("Coin Information", "Wallet: **" + coins + "** coins.", false);
                 XP.setColor(color);
                 channel.sendMessage(XP.build()).queue();
                 break;
@@ -242,95 +252,6 @@ public class CurrencySystem extends ListenerAdapter {
             }
         };
         timer.schedule(task, 1000, 1000);
-    }
-
-    private void Shop(long channel, long member, ShardManager shardmanager, Member user, TextChannel sendchannel) {
-        waiter.waitForEvent(GuildMessageReceivedEvent.class, (event) -> {
-            long nchannel = event.getChannel().getIdLong();
-            long nuser = event.getMember().getUser().getIdLong();
-            return (channel == nchannel && member == nuser);
-        }, (event) -> {
-            String item = event.getMessage().getContentRaw();
-            ArrayList<Long> MemberIDS = new ArrayList<Long>();
-            ArrayList<Integer> MemberMoney = new ArrayList<Integer>();
-            BufferedReader myReader = null;
-            try {
-                myReader = new BufferedReader(new FileReader("MemberMoney"));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            StringTokenizer st = null;
-            String line;
-            try {
-                while ((line = myReader.readLine()) != null) {
-                    st = new StringTokenizer(line);
-
-                    long x = Long.parseLong(st.nextToken());
-                    int z = Integer.parseInt(st.nextToken());
-                    MemberIDS.add(x);
-                    MemberMoney.add(z);
-                }
-                myReader.close();
-
-            } catch (NumberFormatException | IOException e) {
-                e.printStackTrace();
-            }
-
-            boolean found = false;
-            for (int i = 0; i < MemberIDS.size(); i++) {
-                if (MemberIDS.get(i) == member) {
-                    if (item.equalsIgnoreCase("coffee")) {
-                        if (MemberMoney.get(i) >= 500) {
-                            MemberMoney.set(i, MemberMoney.get(i) - 500);
-                            sendchannel.sendMessage("You spent 500 coins on a coffee. It is very delicous!").queue();
-
-                            found = true;
-                            break;
-
-                        } else {
-                            found = true;
-                            sendchannel.sendMessage("I know you are broke, don't try to rob me!").queue();
-                        }
-                    } else {
-                        found = true;
-                        sendchannel.sendMessage("Hey, that's not something you can buy!").queue();
-                    }
-
-                }
-
-            }
-            if (found == false) {
-                MemberIDS.add(member);
-                MemberMoney.add(0);
-                sendchannel.sendMessage("New account registered!").queue();
-
-            }
-            PrintWriter myWriter = null;
-            try {
-                myWriter = new PrintWriter(new BufferedWriter(new FileWriter("MemberMoney")));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            for (int i = 0; i < MemberIDS.size(); i++) {
-
-                String value = String.valueOf(MemberIDS.get(i));
-                String money = String.valueOf(MemberMoney.get(i));
-
-                myWriter.print(value);
-                myWriter.print(" ");
-                myWriter.print(money);
-                myWriter.println();
-
-            }
-            myWriter.close();
-
-        }, 10, TimeUnit.SECONDS, () -> {
-            sendchannel.sendMessage(
-                    "You didn't respond with an object to buy in time! Retry the command if you want to buy something")
-                    .queue();
-            ;
-        });
     }
 
     private int getPlayerTime(User member) {
